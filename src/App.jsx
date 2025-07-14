861959a6ddd056cda4e15fb45fb97ac27877dacd
stanseluos4@gmail.com <stanseluos@gmail.com>
2025-07-14 15:32:51 +0300

    UPDATED FOOTER

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './Components/Navbar';
-import Navbar from './Components/Footer';
 
 
 
