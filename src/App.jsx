9a244377d17b75e91e65927cd0abda46afbeab8e
bando254 <stanseluos4@gmail.com>
2025-07-14 15:06:21 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './Components/Navbar';
-import Footer from './components/footer';
+import Footer from './Components/Footer';
 
 // ✅ Only ONE Footer import
 import Home from './Pages/Home';
